873de0ac67ebe284c68227043c06ac88c8d6be10
Jeonghye Choi <54584063+jeonghye-choi@users.noreply.github.com>
2024-05-12 20:09:35 +0900

    Title: 변수 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/변수 출력하기 3/outputing-variables-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int a = 1;
+    int b = 2;
+    char c = 'C';
+    string insert = "->";
+
+    cout << a << insert << b << insert << c;
+
+    return 0;
+}

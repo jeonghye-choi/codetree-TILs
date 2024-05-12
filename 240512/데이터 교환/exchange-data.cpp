34ffef3f7800def106b137a886560adc13275273
Jeonghye Choi <54584063+jeonghye-choi@users.noreply.github.com>
2024-05-12 20:39:40 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240512/데이터 교환/exchange-data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    int temp = a;
+    a = c;
+    c = b;
+    b = temp;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c;
+
+    return 0;
+}
